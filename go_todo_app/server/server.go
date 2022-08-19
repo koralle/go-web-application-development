8ac1657d9d823564dadcaebc65d6824623fa39ea
koralle <mkhd7930@gmail.com>
2022-08-20 06:43:22 +0900

    graceful-shutdownの実装

--- go_todo_app/server/server.go
+++ go_todo_app/server/server.go
@@ -6,12 +6,17 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/koralle/go-web-application-development/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
 func Run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
 	cfg, err := config.New()
 	if err != nil {
