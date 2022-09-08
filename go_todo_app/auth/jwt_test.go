1a6c51e2563e44f645ab8755d19b2b46cf8a4ce6
koralle <mkhd7930@gmail.com>
2022-09-08 23:24:25 +0900

    GetTokenメソッドに対するテストコード

--- go_todo_app/auth/jwt_test.go
+++ go_todo_app/auth/jwt_test.go
@@ -3,12 +3,28 @@ package auth
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/koralle/go-web-application-development/go_todo_app/clock"
 	"github.com/koralle/go-web-application-development/go_todo_app/entity"
+	"github.com/koralle/go-web-application-development/go_todo_app/store"
 	"github.com/koralle/go-web-application-development/go_todo_app/testutil/fixture"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 )
+
+type FixedTommorowClocker struct{}
+
+func (c FixedTommorowClocker) Now() time.Time {
+	return clock.FixedClocker{}.Now().Add(24 * time.Hour)
+}
 
 func TestEmbed(t *testing.T) {
 	want := []byte("-----BEGIN PUBLIC KEY-----")
@@ -49,6 +65,147 @@ func TestJWTer(t *testing.T) {
 	}
 	if len(got) == 0 {
 		t.Errorf("token is empty")
+	}
+
+}
+
+func TestJWTer_GetToken(t *testing.T) {
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+
+	want, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer("github.com/koralle/go-web-application-development/go_todo_app").
+		Subject("access_token").
+		IssuedAt(c.Now()).
+		Expiration(c.Now().Add(30*time.Minute)).
+		Claim(RoleKey, "test").
+		Claim(UserNameKey, "test_user").
+		Build()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pkey, err := jwk.ParseKey(rawPrivateKey, jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	signed, err := jwt.Sign(want, jwt.WithKey(jwa.RS256, pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	userID := entity.UserID(20)
+
+	ctx := context.Background()
+	moq := &StoreMock{}
+	moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+		return userID, nil
+	}
+
+	sut, err := NewJWTer(moq, c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req := httptest.NewRequest(
+		http.MethodGet,
+		"https://github.com/koralle",
+		nil,
+	)
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", signed))
+	got, err := sut.GetToken(ctx, req)
+	if err != nil {
+		t.Fatalf("want no error, but got %v", err)
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("GetToken() got = %v, want %v", got, want)
+	}
+
+}
+
+func TestJWTer_GEtToken_NG(t *testing.T) {
+	t.Parallel()
+
+	c := clock.FixedClocker{}
+
+	want, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer("github.com/koralle/go-web-application-development/go_todo_app").
+		Subject("access_token").
+		IssuedAt(c.Now()).
+		Expiration(c.Now().Add(30*time.Minute)).
+		Claim(RoleKey, "test").
+		Claim(UserNameKey, "test_user").
+		Build()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pkey, err := jwk.ParseKey(rawPrivateKey, jwk.WithPEM(true))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	signed, err := jwt.Sign(want, jwt.WithKey(jwa.RS256, pkey))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type moq struct {
+		userID entity.UserID
+		err    error
+	}
+
+	tasks := map[string]struct {
+		c   clock.Clocker
+		moq moq
+	}{
+		"expire": {
+			c: FixedTommorowClocker{},
+		},
+		"notFoundInStore": {
+			c: clock.FixedClocker{},
+			moq: moq{
+				err: store.ErrNotFound,
+			},
+		},
+	}
+
+	for n, tt := range tasks {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			moq := &StoreMock{}
+			moq.LoadFunc = func(ctx context.Context, key string) (entity.UserID, error) {
+				return tt.moq.userID, tt.moq.err
+			}
+
+			sut, err := NewJWTer(moq, tt.c)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			req := httptest.NewRequest(http.MethodGet, "https://github.com/koralle", nil)
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", signed))
+
+			got, err := sut.GetToken(ctx, req)
+			if err == nil {
+				t.Errorf("want error, but got nil")
+			}
+
+			if got != nil {
+				t.Errorf("want nil, but got %v", got)
+			}
+		})
 	}
 
 }
